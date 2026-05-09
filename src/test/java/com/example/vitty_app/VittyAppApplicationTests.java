a265842fdc555863ad73fe8183ec6a22c52a8302
KhornMolika <molikakhorn71@gmail.com>
2026-05-09 11:00:59 +0700

    first commit

---
+++ src/test/java/com/example/vitty_app/VittyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vitty_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VittyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
